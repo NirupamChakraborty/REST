008130e9838b95b2dff2b845b8dd69aa24797481
NirupamChakraborty <nirupamchakraborty04@gmail.com>
2025-08-05 19:25:01 +0530

    PASS

--- 2.SqlWithNode/index.js
+++ 2.SqlWithNode/index.js
@@ -1,5 +1,17 @@
 const { faker } = require('@faker-js/faker');
 const mysql = require('mysql2');
+const express = require("express");
+const app = express();
+
+let getRandomUser = ()=> {
+    return [
+      faker.string.uuid(),
+      faker.internet.username(), // before version 9.1.0, use userName()
+      faker.internet.email(),
+      faker.internet.password(),
+    ];
+  }
+//   console.log(getRandomUser());
 
 // Create the connection to database
 const connection = mysql.createConnection({
@@ -9,15 +21,21 @@ const connection = mysql.createConnection({
     password: "Nirupam@01" 
   });
 
+
+
 //  to use the database
 // let q ="SHOW TABLES";
 // imserting new data
 let q ="INSERT INTO user(id, username, email, password) values ?";
-let users =[["5", "1a_newuser", "newa@gmail.com", "ap"], ["3", "3a_newuser", "newa3@gmail.com", "apapap"]];
+// let users =[["5", "1a_newuser", "newa@gmail.com", "ap"], ["3", "3a_newuser", "newa3@gmail.com", "apapap"]];
+let data = [];
+for (let i = 0; i <=100; i++) {
+    data.push(getRandomUser());
+}
 
 try{
     // connection.query("SHOW TABLES", (err, result)=>{
-    connection.query(q,[users],(err, result)=>{
+    connection.query(q,[data],(err, result)=>{
     if(err) throw err;
     console.log(result);
     // console.log(result.length);
@@ -32,12 +50,6 @@ try{
 connection.end();
 
 
-let getRandomUser = ()=> {
-    return {
-      id: faker.string.uuid(),
-      username: faker.internet.username(), // before version 9.1.0, use userName()
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-    };
-  }
-//   console.log(getRandomUser());
+app.listen("8080", ()=>{
+    console.log("server is listening");
+})
